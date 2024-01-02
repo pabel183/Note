ba609cb75544d3e0ec84dc2630b307722b9ca1df
pabel183 <jhpabel183@gmail.com>
2024-01-02 20:25:29 +0600

    search button is activated

--- src/route/pages/Notes.jsx
+++ src/route/pages/Notes.jsx
@@ -1,8 +1,8 @@
-import React,{useContext} from "react";
+import React,{useContext,useEffect,useState} from "react";
 import Card from "../components/Card";
 import SignOUt from "../components/Signout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCirclePlus,faMagnifyingGlass,faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faAngleLeft,faCirclePlus,faMagnifyingGlass,faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
 import MyContext from "../components/MyContext";
@@ -15,37 +15,77 @@ import Cookies from 'js-cookie';
 const Notes = (props) => {
     const navigate=useNavigate();
     const {notes,setNotes,isHeld,setHoldId,setHeld,holdId}=useContext(MyContext);
-    
+    const [isSerachButtonClicked,setSerachButtonClicked]=useState(false);
+    const [isSubmit,setSubmit]=useState(false);
+    const [searchValue,setSearchValue]=useState("");
+    const [tempNotes,setTempNotes]=useState("");
+    useEffect(()=>{
+        setTempNotes(notes)
+    },[tempNotes])
     const deleteNotes=async()=>{
 
         const oldAuthData = Cookies.get("data_validation");          
         await Delete( {data:holdId,selector:oldAuthData} );
-
         const updateNotes=notes.filter((item)=>!holdId.some((holdItem)=>holdItem.id===item.id));
         setNotes(updateNotes);
         setHoldId([]);
         setHeld(false);
     }
 
-    const handleClick=()=>{
-      Swal.fire({
-        text:"upcomming!",
-        position:"top-end",
-        width:"10rem",
-        timer:500,
-        showConfirmButton:false,
-      }
-      );
-    }
+    const handleClick=(event)=>{
+        const name=event.target.getAttribute("name");
+        if(name==="backButton"){
+            // setSerachButtonClicked(false);
+            setNotes(tempNotes);
+            setSubmit(false);
+            console.log("backbutton ");
+        }
+        else{
 
+            setSerachButtonClicked(!isSerachButtonClicked);
+        }
+    }
     
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        setSerachButtonClicked(!isSerachButtonClicked);
+        setSubmit(true);
+    }
+    const handleChange=(event)=>{
+        const value=event.target.value;
+        console.log("search value:");
+        console.log(value);
+        // setTempNotes(notes);
+        const result = tempNotes.filter(obj => Object.values(obj).some(val => val.toString().toLowerCase().includes(value.toLowerCase())));
+        setSearchValue(value);
+        console.log("result: ");
+        console.log(result);
+       
+        if(value===null || value===""){
+            console.log("on the way");
+            console.log(tempNotes);
+            setNotes(tempNotes);
+        }
+        else{
+            console.log("on the other way");
+            setNotes(result);
+        }
+    }
     return (
         <div className="notes">
             <div className="notesHeader">
-                <h1>Notes</h1>
+                {isSubmit
+                ?<FontAwesomeIcon onClick={handleClick} name='backButton' icon={faAngleLeft} className='iconStyle' />
+                :<h1>Notes</h1>}
                 <div className="rightHeader">
-                <SignOUt/>
-                 <FontAwesomeIcon onClick={handleClick} icon={faMagnifyingGlass} className="iconStyle" /> 
+                {   isSerachButtonClicked
+                    ?
+                    <form onSubmit={handleSubmit}>
+                    <input onChange={handleChange} value={searchValue} placeholder="Write something for Serch"/>
+                    </form>
+                    :<SignOUt/>
+                    }
+                 <FontAwesomeIcon onClick={handleClick} name='searchButton' icon={faMagnifyingGlass} className="iconStyle" /> 
                 </div>
             </div>
             <div className={notes.length<6?"controlledContainer":"notesContainer"} >
